8d25c2945810edfb3ace5d851492e2cb8186d0f7
heixhei <2724391331@qq.com>
2023-08-26 14:59:24 +0800

    Spring Boot项目搭建

---
+++ src/main/java/com/baiyuechu/BaiyuechuSpringSecurityTaskApplication.java
@@ -0,0 +1,13 @@
+package com.baiyuechu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaiyuechuSpringSecurityTaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BaiyuechuSpringSecurityTaskApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/baiyuechu/BaiyuechuSpringSecurityTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.baiyuechu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaiyuechuSpringSecurityTaskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
