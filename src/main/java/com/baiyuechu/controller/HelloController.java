e79cd4a8dca25aba9ff64fd1c32431f92d1ec1b3
heixhei <2724391331@qq.com>
2023-08-28 20:16:09 +0800

    编写其他权限方法

--- src/main/java/com/baiyuechu/controller/HelloController.java
+++ src/main/java/com/baiyuechu/controller/HelloController.java
@@ -7,9 +7,20 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
     @GetMapping("/hello")
-    @PreAuthorize("hasAuthority('system:book:update')")
-    public String  hello() {
+    //是否拥有这个权限
+//    @PreAuthorize("hasAuthority('system:book:update')")
+    //是否拥有这些权限
+//    @PreAuthorize("hasAnyAuthority('test','admin','system:book:add')")
+    //是否拥有这个角色
+        @PreAuthorize(("hasRole('system:book:add')"))
+    //是否拥有这些角色
+//    @PreAuthorize(("hasAnyRole('test','admin','system:book:add')"))
+    public String hello() {
         return "hello";
     }
 
+    @GetMapping("/testCors")
+    public String testCors() {
+        return "cors";
+    }
 }
